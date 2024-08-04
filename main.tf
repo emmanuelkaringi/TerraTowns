96a7f4c36e25b60ee905a5118df52cd8f0314ace
Emmanuel Karingi <emmanuelkaringi@gmail.com>
2024-08-04 10:09:23 +0000

    #44 comment code in main.tf

--- main.tf
+++ main.tf
@@ -46,16 +46,16 @@ DESCRIPTION
   content_version = 1
 }
 
-module "home_minions_hosting" {
-  source = "./modules/terrahouse_aws"
-  user_uuid = var.teacherseat_user_uuid
-  #user_uuid = var.user_uuid
-  bucket_name = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  content_version = var.content_version
-  assets_path = var.assets_path
-}
+# module "home_minions_hosting" {
+#   source = "./modules/terrahouse_aws"
+#   user_uuid = var.teacherseat_user_uuid
+#   #user_uuid = var.user_uuid
+#   bucket_name = var.bucket_name
+#   index_html_filepath = var.index_html_filepath
+#   error_html_filepath = var.error_html_filepath
+#   content_version = var.content_version
+#   assets_path = var.assets_path
+# }
 
 # resource "terratowns_home" "home_minions" {
 #   name = "All about Minions"
